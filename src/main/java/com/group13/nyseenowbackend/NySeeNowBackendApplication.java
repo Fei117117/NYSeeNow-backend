60c2834f372624a4c5d8073a29b96dc3f2dd20b6
Fei117117 <katherinekao@icloud.com>
2023-06-20 00:13:01 +0100

    Initial commit

---
+++ src/main/java/com/group13/nyseenowbackend/NySeeNowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group13.nyseenowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NySeeNowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NySeeNowBackendApplication.class, args);
+    }
+
+}

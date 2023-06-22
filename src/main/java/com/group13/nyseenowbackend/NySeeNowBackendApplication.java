007c9e3ac53d42b333770157d5be935871cd9454
Fei117117 <katherinekao@icloud.com>
2023-06-22 16:28:57 +0100

    cors-config

--- src/main/java/com/group13/nyseenowbackend/NySeeNowBackendApplication.java
+++ src/main/java/com/group13/nyseenowbackend/NySeeNowBackendApplication.java
@@ -2,6 +2,9 @@ package com.group13.nyseenowbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NySeeNowBackendApplication {
@@ -10,4 +13,19 @@ public class NySeeNowBackendApplication {
         SpringApplication.run(NySeeNowBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer configure(){
+        return new WebMvcConfigurer(){
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
+            }
+
+        };
+    }
+
 }
+
+
